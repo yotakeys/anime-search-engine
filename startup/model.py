3b64ba3f9303ba1e7f922bf02a5338bf151f9ea1
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2023-02-08 21:28:24 +0700

    feat: bundling dir

---
+++ startup/model.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+from sentence_transformers import SentenceTransformer
+import faiss
+
+class AnimeRecommender():
+    index = faiss.read_index('animes.index')
+    data = pd.read_csv('processed.csv', sep='/')
+    model = SentenceTransformer('model/')
+    
+    query = ""
+    top_k = 0
+    results = list()
+
+    def fetch_anime(self, dataframe_idx):
+        info = self.data.iloc[dataframe_idx]
+        meta = dict()
+        meta['title'] = info['title']
+        return meta
+    
+    def search(self):
+        query_vector = self.model.encode([self.query])
+        self.top_k = self.index.search(query_vector, self.top_k)
+        result_id = self.top_k[1].tolist()[0]
+        result_id = list(np.unique(result_id))
+        results =  [self.fetch_anime(idx) for idx in result_id]
+        return results
+    
+    def recommender(self, query, top_k):
+        self.top_k = top_k
+        self.query = query
+        
+        self.results = self.search()
+        
+        print("Query : ", self.query)
+        print("Result : ")
+        for result  in self.results:
+            print("\t", result)
+    
+if __name__ == '__main__':
+    recommender = AnimeRecommender()
+    recommender.recommender(query = "Anime about isekai using sword", top_k = 5)
